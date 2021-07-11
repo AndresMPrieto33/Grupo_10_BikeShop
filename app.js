def6eec4175ecae5852b4d887f4cc9cfeeb78d8a
AndresMPrieto33 <andresmprieto@outlook.com>
2021-07-11 16:13:33 -0300

    Actualizados los archivos app.js y agregados carrito, detalle, login y registro

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const publicPath = path.resolve(__dirname, 'public');
 app.use (express.static(publicPath));
 
 app.get('/', (req, res)=> {
-    res.sendFile(path.resolve(__dirname, '/views/home.html'))
+    res.sendFile(path.resolve(__dirname, 'public/views/home.html'))
 });
 
 app.listen (3000, () => console.log ('Servidor 3000 corriendo')
