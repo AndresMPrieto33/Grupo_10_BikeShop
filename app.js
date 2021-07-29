78dac04ab354e37d43261e36b773a2186e099bcb
EnriqueKloosterman <kloostermanen@gmail.com>
2021-07-28 21:39:13 -0300

    agragando file olvidado

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
 const express = require ('express');
 const app = express ();
-const path = require ('path');
-const publicPath = path.resolve(__dirname, 'public');
 
-app.set('view engine', 'ejs');
+const path = require ('path');
+
+const publicPath = path.resolve(__dirname, 'public');
 
 app.use (express.static(publicPath));
 
-app.get('/', (req, res)=> {
-    res.sendFile(path.join(__dirname, 'public/views/home.html'))
-});
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+//Require de los archivos de las rutas
+const mainRouter = require('./routes/main');
+
+
+app.use('/', mainRouter);
+
 
 app.listen (3000, () => console.log ('Servidor 3000 corriendo')
 );
-
-app.get('/login', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'public/views/login.html'));
-});
-
-app.get('/registro', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'public/views/registro.html'));
-});
-
-app.get('/Carrito', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'public/views/Carrito.html'));
-});
-
-app.get('/detalle', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'public/views/detalle.html'));
-});
