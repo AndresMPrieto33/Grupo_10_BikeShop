1203ae1c2745bb6aeaccce64cc5303837995798c
AndresMPrieto33 <andresmprieto@outlook.com>
2021-08-29 17:14:19 -0300

    Registro de users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const fs = require('fs');
+
+const userFilePath = path.join(__dirname, '../data/users.json');
+const user = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
 
 const userController = {
     register: (req, res) => {
@@ -7,8 +11,23 @@ const userController = {
     login: (req, res) => {
         res.render('login')
     },
-    admin: (req, res)=>{
+    admin: (req, res) => {
         res.render('admin');
+    },
+    store: (req, res) => {
+        let nuevoId = user[user.length - 1].id + 1;
+        let id = nuevoId;
+        let nuevoUsuario = {
+            name: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            direccion: req.body.direccion,
+            id: nuevoId,
+            imagenPerfil: req.file.originalname,
+        }
+        user.push(nuevoUsuario);
+        fs.writeFileSync(userFilePath, JSON.stringify(user));
+        res.redirect('/')
     }
 }
 
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({ 
+    destination: function (req, file, cb) { 
+       cb(null, path.resolve(__dirname, '../public/images/users')); 
+    }, 
+    filename: function (req, file, cb) { 
+       cb(null, file.originalname) } 
+  });
+
+let upload = multer({ storage: storage });
+
 
 const userController = require('../controllers/userController');
 
 router.get('/register', userController.register);
+router.post('/store', upload.single('image'), userController.store);
 router.get('/login', userController.login);
 router.get('/admin', userController.admin);
+
 
 module.exports = router;
