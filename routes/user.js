2db8483b345846e863db28f18773895d7453b2e2
EnriqueKloosterman <kloostermanen@gmail.com>
2021-09-02 16:56:53 -0300

    rtq

--- routes/user.js
+++ routes/user.js
@@ -4,12 +4,12 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({ 
-    destination: function (req, file, cb) { 
-       cb(null, path.resolve(__dirname, '../public/images/users')); 
-    }, 
-    filename: function (req, file, cb) { 
-       cb(null, file.originalname) } 
-  });
+   destination: function (req, file, cb) { 
+      cb(null, path.resolve(__dirname, '../public/images/users')); 
+   }, 
+   filename: function (req, file, cb) { 
+      cb(null, file.originalname) } 
+   });
 
 let upload = multer({ storage: storage });
 
