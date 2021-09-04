2736b2a9c6a7123545b1126b06fb424a1b94d056
EnriqueKloosterman <kloostermanen@gmail.com>
2021-09-04 12:47:15 -0300

    acymv

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const { validationResult} = require('express-validator');
 
 const userFilePath = path.join(__dirname, '../data/users.json');
 const user = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
@@ -14,20 +15,30 @@ const userController = {
     admin: (req, res) => {
         res.render('admin');
     },
-    store: (req, res) => {
+    storage: (req, res) => {
+        // const resutlValidation = validationResult(req);
+
+        // if(resutlValidation.errors.length > 0){
+        //     return res.render('register', {
+        //         errors: resutlValidation.mapped(),
+        //         oldData: req.body
+        //     });
+        // }
+
         let nuevoId = user[user.length - 1].id + 1;
         let id = nuevoId;
         let nuevoUsuario = {
+            id: nuevoId,
             name: req.body.firstName,
             lastName: req.body.lastName,
             email: req.body.email,
-            direccion: req.body.direccion,
-            id: nuevoId,
+            address: req.body.address,
+            caracteristicas: req.body.caracteristicas,
             imagenPerfil: req.file.originalname,
         }
         user.push(nuevoUsuario);
-        fs.writeFileSync(userFilePath, JSON.stringify(user));
-        res.redirect('/')
+        fs.writeFileSync(userFilePath, JSON.stringify(user, null, 2));
+        return res.redirect('/user/login');
     }
 }
 
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
 const path = require('path');
+const multer = require('multer');
+const { body } = require('express-validator');
 
 const storage = multer.diskStorage({ 
    destination: function (req, file, cb) { 
@@ -11,13 +12,38 @@ const storage = multer.diskStorage({
       cb(null, file.originalname) } 
    });
 
-let upload = multer({ storage: storage });
-
+const upload = multer({ storage: storage });
 
 const userController = require('../controllers/userController');
 
+// const validations = [
+//    body('firstName').notEmpty().withMessage('Tienes que ingresar tu nombre'),
+//    body('lastName').notEmpty().withMessage('Tienes que ingresar tu apellido'),
+//    body('email').notEmpty().withMessage('Ingresa tu email').bail().isEmail().withMessage('Debes ingresar un formato de email valido'),
+//    body('address').notEmpty().withMessage('Ingresa una dirección'),
+//    body('password').notEmpty().withMessage('Debes escribir una contraseña'),
+//    body('image').custom((value, { req }) => {
+//       let file = req.file;
+//       let acceptedExtentions = ['.jpg', '.png', '.gif'];
+//       if(!file){
+//          throw new Error('Tienes que subir una imagen');
+//       }else{
+//          let fileExtention = path.extname(file.originalname);
+//          if(!acceptedExtentions.includes(fileExtention)){
+//             throw new Error(`Las extenciones de arcivo permitidas son ${acceptedExtentions.join(', ')}`)
+//          }
+
+//       }
+//       return true;
+// })
+
+// ]
+
+//FORMUÑARIO DE REGISTRO
 router.get('/register', userController.register);
-router.post('/store', upload.single('image'), userController.store);
+router.post('/storage', upload.single('image'), userController.storage);
+
+//LOGIN
 router.get('/login', userController.login);
 router.get('/admin', userController.admin);
 
