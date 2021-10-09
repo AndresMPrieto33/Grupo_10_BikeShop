51242cf391d714741e882e4bbe9473daca424b87
EnriqueKloosterman <kloostermanen@gmail.com>
2021-10-09 11:49:04 -0300

    controlladores vistas por categorias

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const fs =require('fs');
+//Requiere de la DB
+const db = require('../database/models');
+const sequelize = db.sequelize;
+
 
 const productsFilePath = path.join(__dirname, '../data/productsDB.json');
 let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-//Requiere de la DB
-const db = require('../database/models');
 
 const productsController = {
     products: (req, res) => {
@@ -16,24 +18,54 @@ const productsController = {
         }) 
     },
 
-    /*linea vieja
-    res.render('productsAll', {products: products});
-    */
-    bikes: (req, res) => {
-        let bikes = products.filter(element => element.type === "bicicletas");
-        res.render('bikes', {bikes: bikes});
+    detail: (req, res) =>{
+        db.Product.findByPk(req.params.id)
+        .then(products => {
+            res.render('detail', {products: products})
+        })
     },
-    accesorios:(req, res) =>{
-        let accesorios = products.filter(element => element.type === "accesorios");
-        res.render('accesorios', {accesorios: accesorios});
+
+    bikes: (req, res) =>{
+        db.Product.findAll({
+            where: {
+                category: 1
+            }
+        })
+        .then(bikes => {
+            res.render('bikes', {bikes})
+        })
     },
+
+    accesorios: (req, res) =>{
+        db.Product.findAll({
+            where: {
+                category: 2
+            }
+        })
+        .then(accesorios => {
+            res.render('accesorios', {accesorios})
+        })
+    },
+    
     parts: (req, res) => {
-        let parts = products.filter(element => element.type === "partes");
-        res.render('parts', {parts: parts});
+        db.Product.findAll({
+            where: {
+                category: 3
+            }
+        })
+        .then(parts => {
+            res.render('parts', {parts})
+        })
     },
     ruedas: (req, res) =>{
-        let ruedas = products.filter(element => element.type === "ruedas");
-        res.render('ruedas', {ruedas: ruedas});
+        db.Product.findAll({
+            where:{
+                category: 5
+            }
+        })
+        .then(ruedas=> {
+            res.render('ruedas', {ruedas: ruedas})
+        })
     },
     sale: (req, res) => {
         let sale = products.filter(element => element.category === "sale");
@@ -42,36 +74,10 @@ const productsController = {
     cart: (req, res) => {
         res.render('cart');
     },
-    detail: (req, res) => {
-        db.Product.findByPk(req.params.id) 
-            .then(function (product) {
-                res.render('detail', {product: product});
-            })
-        /*
-        let id = req.params.id;
-		let detalle = products.find(element => element.id == id);
-		res.render('detail', {detalle: detalle});
-        */
-    },
+    
     nuevo: (req, res) => {
 		res.render('productCreate');
 	},
-    // store: (req, res) => {
-    //     let nuevoId = products[products.length - 1].id + 1;
-	// 	let id = nuevoId;
-	// 	let nuevoProducto = {
-	// 		name: req.body.name,
-	// 		price: req.body.price,
-	// 		category: req.body.category,
-	// 		discount: req.body.discount,
-	// 		id: nuevoId,
-	// 		image: req.file.originalname,
-	// 	}
-
-	// 	products.push(nuevoProducto);
-	// 	fs.writeFileSync(productsFilePath, JSON.stringify(products));
-	// 	res.redirect('/products/detail/' + id)
-    // },
     create: (req, res) => {
         let newProduct = req.body;
         db.Product.create({
@@ -80,12 +86,11 @@ const productsController = {
             discount: req.body.discount,
             description: req.body.description,
             stock: req.body.stock,
-            //brand: req.body.brand,
+            brand: req.body.brand,
             image: req.file.originalname,
             product_detail: req.body.size,
-            category: req.body.category
-            //size: req.body.size,
-            //color: req.body.color
+            category: req.body.category,
+            color: req.body.color
         })
         .then(()=>{
             res.redirect('/')
