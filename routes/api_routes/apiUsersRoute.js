9bd4b7513a20c134017a34a8acb0881d77f8f4b4
pedromartinrodriguez <pedromrodriguez@gmail.com>
2021-11-08 09:18:26 -0300

    api users detail

--- routes/api_routes/apiUsersRoute.js
+++ routes/api_routes/apiUsersRoute.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const apiUsersController = require('../../controllers/api_controllers/apiUsersController');
 
 router.get('/', apiUsersController.users);
+router.get('/:id', apiUsersController.detail);
 
 
 
-module.exorts = router;
+module.exports = router;
