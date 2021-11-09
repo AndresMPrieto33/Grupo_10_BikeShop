5a1bb1fd6027d7cb18d333a82adf51025e1cec3b
EnriqueKloosterman <kloostermanen@gmail.com>
2021-11-08 22:47:15 -0300

    rtq

--- controllers/api_controllers/apiProuductsController.js
+++ controllers/api_controllers/apiProuductsController.js
@@ -31,4 +31,79 @@ module.exports = {
                 })
                 .catch(error => res.json(error));
     },
+    bikes: (req, res) =>{
+        db.Product
+                .findAll({
+                    where: {
+                        category: 1
+                    }
+                })
+                .then(products => {
+                    return res.status(200).json({
+                        meta:{
+                            status: 200,
+                            totalProducts: products.length,
+                            url: 'api/products/bikes'
+                        },
+                        data: products
+                    })
+                })
+                .catch(error => res.json(error));
+    },
+    accesorios: (req, res) =>{
+        db.Product.findAll({
+                    where: {
+                        category: 2
+                    }
+                })
+                .then(products => {
+                    return res.status(200).json({
+                        meta: {
+                            status: 200,
+                            totalProducts: products.length,
+                            url: 'api/products/accesorios'
+                        },
+                        data: products
+                    })
+                })
+                .catch(error => res.json(error))
+    },
+    parts: (req, res) =>{
+        db.Product.findAll({
+                    where: {
+                        category: 3
+                    }
+                })
+                .then(products => {
+                    return res.status(200).json({
+                        meta: {
+                            status: 200,
+                            totalProducts: products.length,
+                            url: 'api/products/parts'
+                        },
+                        data: products
+                    })
+                })
+                .catch(error => res.json(error))
+    },
+    ruedas: (req, res) =>{
+        db.Product.findAll({
+                    where: {
+                        category: 5
+                    }
+                })
+                .then(products => {
+                    return res.status(200).json({
+                        meta: {
+                            status: 200,
+                            totalProducts: products.length,
+                            url: 'api/products/ruedas'
+                        },
+                        data: products
+                    })
+                })
+                .catch(error => res.json(error))
+    }
+
+
 }
--- routes/api_routes/apiProductsRoute.js
+++ routes/api_routes/apiProductsRoute.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const apiProductsController = require('../../controllers/api_controllers/apiProuductsController');
 
 router.get('/', apiProductsController.products);
+router.get('/bikes/', apiProductsController.bikes);
+router.get('/accesorios', apiProductsController.accesorios);
+router.get('/parts', apiProductsController.parts);
+router.get('/ruedas', apiProductsController.ruedas);
 router.get('/:id/', apiProductsController.detail);
 
 
